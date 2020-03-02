8c909d7987d0d5e10372a8aa591074c6d3973d2d
Lucas Carvalho <lucascarvalho@gmail.com>
2020-03-01 17:53:09 -0800

    Add support to typescript imports

--- rules/import.js
+++ rules/import.js
@@ -4,7 +4,24 @@
 module.exports = {
   extends: [
     "plugin:import/errors",
-    "plugin:import/typescript",
     "plugin:import/warnings",
+    "plugin:import/typescript",
   ],
+  plugins: ["import"],
+  rules: {
+    // Turn on errors for missing imports because it conflicts with TypeScript configuration
+    "import/no-unresolved": "error",
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      // Use <root>/tsconfig.json
+      typescript: {
+        // Always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        alwaysTryTypes: true,
+      },
+    },
+  },
 };
