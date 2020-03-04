85a5ce667c391648bfbc4799e8a6eae7b98e6d9c
Lucas Carvalho <lucascarvalho@gmail.com>
2020-03-03 21:22:20 -0800

    Move rules to the correct files

--- rules/typescript.js
+++ rules/typescript.js
@@ -3,4 +3,8 @@
  */
 module.exports = {
   extends: ["plugin:@typescript-eslint/all"],
+  rules: {
+    /* Disable for common numbers */
+    "@typescript-eslint/no-magic-numbers": ["error", { ignore: [-1, 0, 1] }],
+  },
 };
