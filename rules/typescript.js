1a3de6774a14b5c35e03aea72afc52964ddc51eb
Lucas Carvalho <lucascarvalho@gmail.com>
2020-03-28 09:37:33 -0700

    Disable some import rules for configuration files

--- rules/typescript.js
+++ rules/typescript.js
@@ -3,6 +3,16 @@
  */
 module.exports = {
   extends: ["plugin:@typescript-eslint/all"],
+  overrides: [
+    /* Disable import rules that are commonly used in configuration files. */
+    {
+      files: ["*.config.js"],
+      rules: {
+        "@typescript-eslint/no-require-imports": "off",
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+  ],
   rules: {
     /* Disable for common numbers. */
     "@typescript-eslint/no-magic-numbers": ["error", { ignore: [-1, 0, 1] }],
