dbe21db70d53229cf0f3e4271080f6db028338ed
Lucas Carvalho <lucascarvalho@gmail.com>
2020-03-31 01:19:09 -0700

    Update react.js configuration

--- rules/react.js
+++ rules/react.js
@@ -5,8 +5,23 @@ module.exports = {
   extends: ["plugin:react/all"],
   plugins: ["react"],
   rules: {
-    /* Allow .tsx externsion */
+    /* Default components to arrow function. */
+    "react/function-component-definition": [
+      "error",
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+
+    /* Allow .tsx extension. */
     "react/jsx-filename-extension": [1, { extensions: [".tsx", ".jsx"] }],
+
+    /* Avoid conflict with other rule to format strings in JSX.  */
+    "react/jsx-no-literals": [
+      "off",
+      { allowedStrings: ["allowed"], ignoreProps: false, noStrings: true },
+    ],
   },
   settings: {
     react: {
